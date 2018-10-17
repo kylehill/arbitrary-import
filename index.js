378200115a35487f326a4f562d603952d74cb32f
Kyle Hill <kyhill@usnews.com>
2018-10-17 15:09:49 -0400

    Adds some comments to index.js

--- index.js
+++ index.js
@@ -6,32 +6,59 @@ const async = require("async")
 
 // project code
 const code = require("./code")
-const env = require("./env")
+const env = require("./env") // Postgres location and credentials
 
 const start = Date.now()
 
 async.auto({
 
+    // Reads all files in the /specs directory into memory
     spec_files: (cb) => {
         code.read_directory(path.resolve(__dirname, "specs"), cb)
     },
 
+    // Reads all files in the /data directory into memory
+    /* Returns an array of objects with the shape { 
+        filename: formatName1_date.txt,
+        format: formatName1,
+        content: [lines of text] 
+    } */
     data_files: (cb) => {
         code.read_directory(path.resolve(__dirname, "data"), cb)
     },
 
+    // Runs all the files in the /specs directory through a CSV parser
+    /* Returns an object with the shape { 
+        formatName1: [columns], 
+        formatName2: [columns], ... 
+    } */
     parse_specs: [ "spec_files", (results, cb) => {
         code.parse_specs(results.spec_files, cb)
     }],
 
+    // Runs all the files in the /data directory through the appropriate de-serializer
+    /* Returns an array of objects with the shape {
+        filename: <same>
+        format: <same>
+        data: [
+            { column1: "X", column2: true, column3: 45 }, ...
+        ]
+    } */
     parse_data: [ "data_files", "parse_specs", (results, cb) => {
         code.parse_data(results.data_files, results.parse_specs, cb)
     }],
 
+    // Creates a schema and tables in the specified postgres instance
+    // Returns the schema's name
     create_structure: [ "parse_data", (results, cb) => {
         code.create_structure(results.parse_specs, results.parse_data, env, cb)
     }],
 
+    // Inserts all rows into their applicable tables
+    /* Returns an object with the shape { 
+        tables: 13,
+        rows: 254
+    } */
     insert_rows: [ "create_structure", (results, cb) => {
         code.insert_rows(
             results.parse_specs, 
